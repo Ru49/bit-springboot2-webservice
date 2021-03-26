a4d1376abdd0031c1c9eb74b648f4fd75f75fd60
Ru49 <dpslah042@gmail.com>
2021-03-26 15:16:47 +0900

    commit

---
+++ src/main/java/com/example/web/springboot/web/PostsApiController.java
@@ -0,0 +1,38 @@
+package com.example.web.springboot.web;
+
+import com.example.web.springboot.service.PostsService;
+import com.example.web.springboot.web.dto.PostsResponseDto;
+import com.example.web.springboot.web.dto.PostsSaveRequestDto;
+import com.example.web.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long update(@PathVariable PostsSaveRequestDto requestDto){
+
+        return postsService.save(requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
